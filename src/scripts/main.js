e5b8094d053e872ce6c9f4c270188edfd1a33498
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-04-20 09:25:19 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
+'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  // 2. Прив’язуємо label до input через id
+  label.setAttribute('for', input.id);
+
+  const text = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  label.textContent = text;
+
+  input.placeholder = text;
+
+  input.parentElement.prepend(label);
+});
