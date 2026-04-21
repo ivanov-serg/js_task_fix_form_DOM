e38a70dc35041ee41f3a066fc2fee36f9d208b7f
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-04-21 15:43:30 +0300

    make build

---
+++ js_task_fix_form_DOM.dff005d4.js
@@ -0,0 +1,2 @@
+"use strict";var forms=document.querySelectorAll("form");forms.forEach(function(e){e.querySelectorAll("input").forEach(function(e){var t=document.createElement("label");t.classList.add("field-label"),t.setAttribute("for",e.id);var r=e.name.charAt(0).toUpperCase()+e.name.slice(1);t.textContent=r,e.placeholder=r,e.parentElement.prepend(t)})});
+//# sourceMappingURL=js_task_fix_form_DOM.dff005d4.js.map
